4d79bed4a17fd7f09b7ef9e8e31d1cc546165e2a
Adrian Teng-Amnuay <pumpadrian@gmail.com>
2021-07-15 08:51:41 -0700

    Update read me. replace hardcoded ints with slashcommandoptiontype

--- README.md
+++ README.md
@@ -2,19 +2,47 @@
 
 Discord bot written with `discord.py` hosted on `Heroku` 
 
-## Install
+## Getting Started
+
+### Prerequisites
+1. Download Python3
+1. Download an IDE like
+    - **Atom**
+    - **Visual Studio Code** (better)
+    - **PyCharm** (best)
+1. Install virtualenv using `pip install -U virtualenv`
+
+### Setup the project
 ```
-virtualenv venv
+# Create the virtualenv
+virtualenv venv        
+
+# Activate the virtualenv
 source venv/bin/activate    # Linux / OSX
 venv/Scripts/activate.ps1   # Windows powershell
+
+# Install requirements
 pip install -r requirements.txt
 ```
 
-## Run locally
+### Run locally
+The bot is configured using *Environment Variables*
 ```
-BOT_TOKEN=token python client.py
-APP_ENV=production BOT_TOKEN=token python client.py     # set APP_ENV=production to sync commands
+# Setting APP_ENV=production will sync slash commands
+[APP_ENV=production] BOT_TOKEN=token python client.py
 ```
 
+### Set up linting
+- [PyCharm](https://plugins.jetbrains.com/plugin/11084-pylint)
+- [Visual Studio Code](https://code.visualstudio.com/docs/python/linting)
+- [Atom](https://hackernoon.com/setting-up-atom-as-a-python-ide-a-how-to-guide-o6dd37ff)
+
+### Learn how to run the debugger
+- [PyCharm](https://www.jetbrains.com/help/pycharm/debugging-your-first-python-application.html)
+- [Visual Studio Code](https://code.visualstudio.com/docs/python/python-tutorial)
+- [Atom](https://atom.io/packages/python-debugger)
+
 ## Useful reading
-<https://discord-py-slash-command.readthedocs.io/en/latest/>
+- Documentation for [discord-py-slash-command](https://discord-py-slash-command.readthedocs.io/en/latest/)
+- Documentation for [discord.py](https://discordpy.readthedocs.io/en/stable/)
+- Documentation for [discord apps](https://discord.com/developers/docs/intro)
--- client.py
+++ client.py
@@ -8,14 +8,17 @@ from discord import Intents, Embed
 from discord.colour import Color
 from discord.ext import commands
 from discord_slash import SlashCommand, SlashContext
+import discord_slash
+from discord_slash.model import SlashCommandOptionType
 from discord_slash.utils.manage_commands import create_option
 
 
 APP_ENV = os.getenv('APP_ENV', "")
 BOT_TOKEN = os.getenv('BOT_TOKEN')
-guild_ids = [218962182606422016] # bay of pigs
+guild_ids = [218962182606422016]  # bay of pigs
 
-bot = commands.Bot(command_prefix='!', description="the pork bot", intents=Intents.all())
+bot = commands.Bot(command_prefix='!',
+                   description="the pork bot", intents=Intents.all())
 slash = SlashCommand(bot, sync_commands=APP_ENV.lower() == 'production')
 
 
@@ -30,23 +33,23 @@ async def _ping(ctx):
 
 
 @slash.slash(
-  name="roll",
-  guild_ids=guild_ids,
-  description="Roll an N-sided die",
-  options=[
-    create_option(
-      name="num_sides",
-      description="How many sides to the die? (default: 6)",
-      option_type=4,
-      required=False,
-    ),
-    create_option(
-      name="num_dice",
-      description="How many die to roll? (default: 2)",
-      option_type=4,
-      required=False,
-    ),
-  ])
+    name="roll",
+    guild_ids=guild_ids,
+    description="Roll an N-sided die",
+    options=[
+        create_option(
+            name="num_sides",
+            description="How many sides to the die? (default: 6)",
+            option_type=SlashCommandOptionType.INTEGER,
+            required=False,
+        ),
+        create_option(
+            name="num_dice",
+            description="How many die to roll? (default: 2)",
+            option_type=SlashCommandOptionType.INTEGER,
+            required=False,
+        ),
+    ])
 async def roll(ctx: SlashContext, num_sides: int = 6, num_dice: int = 2):
   rolls = []
   for _ in range(num_dice):
@@ -56,7 +59,7 @@ async def roll(ctx: SlashContext, num_sides: int = 6, num_dice: int = 2):
 
 @slash.slash(name="sysinfo", guild_ids=guild_ids, description="Print the bot system information")
 async def system_info(ctx: SlashContext):
-  embed = Embed(title="Platform", color=Color.blurple())
+  embed = Embed(title="Platform Info", color=Color.blurple())
   embed.add_field(name="System", value=platform.system(), inline=False)
   embed.add_field(name="Release", value=platform.release(), inline=False)
   embed.add_field(name="Version", value=platform.version(), inline=False)
@@ -66,13 +69,18 @@ async def system_info(ctx: SlashContext):
 @slash.slash(name="version", guild_ids=guild_ids, description="Print the app version information")
 async def version(ctx: SlashContext):
   try:
-    dt = datetime.strptime(os.getenv("HEROKU_RELEASE_CREATED_AT"), "%Y-%m-%dT%H:%M:%SZ")
+    dt = datetime.strptime(
+        os.getenv("HEROKU_RELEASE_CREATED_AT"), "%Y-%m-%dT%H:%M:%SZ")
   except ValueError:
     dt = None
-  embed = Embed(title="Deployment", timestamp=dt, color=Color.blurple())
-  embed.add_field(name="Release Version", value=os.getenv("HEROKU_RELEASE_VERSION"), inline=False)
-  embed.add_field(name="Commit", value=os.getenv("HEROKU_SLUG_COMMIT"), inline=False)
-  embed.add_field(name="Description", value=os.getenv("HEROKU_SLUG_DESCRIPTION"), inline=False)
+  embed = Embed(title="Deployment Info", timestamp=dt, color=Color.blurple())
+  embed.add_field(name="Release Version", value=os.getenv(
+      "HEROKU_RELEASE_VERSION"), inline=False)
+  embed.add_field(name="Commit", value=os.getenv(
+      "HEROKU_SLUG_COMMIT"), inline=False)
+  embed.add_field(name="Description", value=os.getenv(
+      "HEROKU_SLUG_DESCRIPTION"), inline=False)
   await ctx.send(embed=embed)
+
 
 bot.run(BOT_TOKEN)
