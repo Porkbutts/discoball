d7fb9d3f97f583a415b6f83ce91746fd5b02f0f8
Adrian Teng-Amnuay <pumpadrian@gmail.com>
2021-07-14 22:48:35 -0700

    move description back down

--- client.py
+++ client.py
@@ -23,9 +23,11 @@ slash = SlashCommand(bot, sync_commands=APP_ENV.lower() == 'production')
 async def on_ready():
     print("Ready!")
 
+
 @slash.slash(name="ping", guild_ids=guild_ids, description="Ping the bot")
 async def _ping(ctx):
     await ctx.send(f"Pong! ({bot.latency*1000}ms)")
+
 
 @slash.slash(
   name="roll",
@@ -51,23 +53,26 @@ async def roll(ctx: SlashContext, num_sides: int = 6, num_dice: int = 2):
     rolls.append(random.randint(1, num_sides))
   await ctx.send(content=f"Rolled {num_dice} {num_sides}-sided dice. Output: {rolls}, Total: {sum(rolls)}")
 
-@slash.slash(name="sysinfo", guild_ids=guild_ids)
+
+@slash.slash(name="sysinfo", guild_ids=guild_ids, description="Print the bot system information")
 async def system_info(ctx: SlashContext):
-  embed = Embed(title="System Information", color=Color.blurple())
+  embed = Embed(title="Platform", color=Color.blurple())
   embed.add_field(name="System", value=platform.system(), inline=False)
   embed.add_field(name="Release", value=platform.release(), inline=False)
   embed.add_field(name="Version", value=platform.version(), inline=False)
   await ctx.send(embed=embed)
 
-@slash.slash(name="version", guild_ids=guild_ids)
+
+@slash.slash(name="version", guild_ids=guild_ids, description="Print the app version information")
 async def version(ctx: SlashContext):
   try:
     dt = datetime.strptime(os.getenv("HEROKU_RELEASE_CREATED_AT"), "%Y-%m-%dT%H:%M:%SZ")
   except ValueError:
     dt = None
-  embed = Embed(title="Version Information", timestamp=dt, color=Color.blurple(), description=os.getenv("HEROKU_SLUG_DESCRIPTION"))
+  embed = Embed(title="Deployment", timestamp=dt, color=Color.blurple())
   embed.add_field(name="Release Version", value=os.getenv("HEROKU_RELEASE_VERSION"), inline=False)
   embed.add_field(name="Commit", value=os.getenv("HEROKU_SLUG_COMMIT"), inline=False)
+  embed.add_field(name="Description", value=os.getenv("HEROKU_SLUG_DESCRIPTION"), inline=False)
   await ctx.send(embed=embed)
 
 bot.run(BOT_TOKEN)
